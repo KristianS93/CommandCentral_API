50e27084a05d52d2174eb8cb022305758883e1ef
Kristian <KristianS93@hotmail.com>
2023-07-02 16:02:32 +0200

    Added dependency injection for the dbcontext, and fixes dockerfile.

--- CommandCentralAPI/WebApi/API/Program.cs
+++ CommandCentralAPI/WebApi/API/Program.cs
@@ -12,7 +12,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddDbContext<ApiDbContext>(options =>
+builder.Services.AddDbContext<IApiDbContext, ApiDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("Postgres")));
 
 builder.Services.AddScoped<IHouseholdService, HouseholdService>();
