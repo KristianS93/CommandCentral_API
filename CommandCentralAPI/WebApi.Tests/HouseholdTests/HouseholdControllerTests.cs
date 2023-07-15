6e570947ecbfe5bfd12ad9f7339ddfced04dfa6a
Kristian <KristianS93@hotmail.com>
2023-07-15 14:49:35 +0200

    household controller test

--- CommandCentralAPI/WebApi.Tests/HouseholdTests/HouseholdControllerTests.cs
+++ CommandCentralAPI/WebApi.Tests/HouseholdTests/HouseholdControllerTests.cs
@@ -1,42 +1,42 @@
-// using API.Controllers;
-// using DatabaseFixture.Tests;
-// using Domain.Entities;
-// using Infrastructure.Services;
-// using Microsoft.AspNetCore.Mvc;
-// using Microsoft.Extensions.Logging;
-// using Moq;
-//
-// namespace WebApi.Tests.HouseholdTests;
-//
-// public class HouseholdControllerTests : IClassFixture<TestDatabaseFixture>
-// {
-//     public HouseholdControllerTests(TestDatabaseFixture fixture)
-//     {
-//         Fixture = fixture;
-//     }
-//     public TestDatabaseFixture Fixture { get; set; }
-//
-//     [Fact]
-//     public async void GetHouseholds_ListOfIds()
-//     {
-//         // Arrange 
-//         using var context = Fixture.CreateContext();
-//         var loggerMockController = Mock.Of<ILogger<HouseholdController>>();
-//         var loggerMockService= Mock.Of<ILogger<HouseholdService>>();
-//         var service = new HouseholdService(context, loggerMockService);
-//         var controller = new HouseholdController(loggerMockController, service);
-//         var expected = new List<HouseholdEntity>
-//         {
-//             new HouseholdEntity { Id = 1, Name = "Kristians hus" },
-//             new HouseholdEntity { Id = 2, Name = "Ibis hus" }
-//         };
-//         
-//         // Act
-//         var actualHouseholds = await controller.GetHouseholds();
-//         var actual = actualHouseholds.Result as OkObjectResult;
-//         // Assert
-//         Assert.Equivalent(expected, actual.Value);
-//
-//     }
-//     
-// }
+using API.Controllers;
+using DatabaseFixture.Tests;
+using Domain.Entities;
+using Infrastructure.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace WebApi.Tests.HouseholdTests;
+
+public class HouseholdControllerTests : IClassFixture<TestDatabaseFixture>
+{
+    public HouseholdControllerTests(TestDatabaseFixture fixture)
+    {
+        Fixture = fixture;
+    }
+    public TestDatabaseFixture Fixture { get; set; }
+
+    [Fact]
+    public async void GetHouseholds_ListOfIds()
+    {
+        // Arrange 
+        using var context = Fixture.CreateContext();
+        var loggerMockController = Mock.Of<ILogger<HouseholdController>>();
+        var loggerMockService= Mock.Of<ILogger<HouseholdService>>();
+        var service = new HouseholdService(context, loggerMockService);
+        var controller = new HouseholdController(loggerMockController, service);
+        var expected = new List<HouseholdEntity>
+        {
+            new HouseholdEntity { Id = 1, Name = "Kristians hus" },
+            new HouseholdEntity { Id = 2, Name = "Ibis hus" }
+        };
+        
+        // Act
+        var actualHouseholds = await controller.GetHouseholds();
+        var actual = actualHouseholds.Result as OkObjectResult;
+        // Assert
+        Assert.Equivalent(expected, actual.Value);
+
+    }
+    
+}
