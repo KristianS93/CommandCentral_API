8a92fc5fc15f61ad70801a0b639fe005fd17d207
Kristian <KristianS93@hotmail.com>
2023-07-27 01:13:21 +0200

    Adding db connection for azure, while keeping local development, Close #54

--- CommandCentralAPI/WebApi/API/Extensions/PersistenceExtension.cs
+++ CommandCentralAPI/WebApi/API/Extensions/PersistenceExtension.cs
@@ -7,8 +7,15 @@ public static class PersistenceExtension
 {
     public static IServiceCollection AddPersistence(this IServiceCollection service, ConfigurationManager conf)
     {
+        var connectionString =
+            Environment.GetEnvironmentVariable("POSTGRESQLCONNSTR_AZURE_POSTGRESQL_CONNECTIONSTRING");
+        if (String.IsNullOrEmpty(connectionString))
+        {
+            // Local Development
+            connectionString = conf.GetConnectionString("Postgres");
+        }
         service.AddDbContext<IApiDbContext, ApiDbContext>(options =>
-            options.UseNpgsql(conf.GetConnectionString("Postgres")));
+            options.UseNpgsql(connectionString));
         return service;
     }
 }
