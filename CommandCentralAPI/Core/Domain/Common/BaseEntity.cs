a9b09307afda76c173ce2753c64351e4c804e17b
Kristian <KristianS93@hotmail.com>
2023-08-07 00:23:12 +0200

    Adding domain for #67

--- CommandCentralAPI/Core/Domain/Common/BaseEntity.cs
+++ CommandCentralAPI/Core/Domain/Common/BaseEntity.cs
@@ -3,6 +3,9 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Domain.Common;
 
+/// <summary>
+/// Base entity enabling id, and datetime for creation.
+/// </summary>
 public abstract class BaseEntity
 {
     public int Id { get; set; }
