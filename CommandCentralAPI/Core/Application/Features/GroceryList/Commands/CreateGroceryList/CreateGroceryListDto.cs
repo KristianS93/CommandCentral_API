629f68b12284f3e09311327fcc6c20042e0f3676
Desktop <KristianS93@hotmail.com>
2023-09-16 00:30:08 +0200

    Changed CreateGroceryListDto to a record

--- CommandCentralAPI/Core/Application/Features/GroceryList/Commands/CreateGroceryList/CreateGroceryListDto.cs
+++ CommandCentralAPI/Core/Application/Features/GroceryList/Commands/CreateGroceryList/CreateGroceryListDto.cs
@@ -1,6 +1,6 @@
 namespace Application.Features.GroceryList.Commands.CreateGroceryList;
 
-public class CreateGroceryListDto
+public record CreateGroceryListDto
 {
     public int Id { get; set; }
     public DateTime CreatedAt { get; set; }
